50782f06452e55e513b2c3c6ea1a74070eec038f
Anja Sheppard <anjashep@umich.edu>
2023-11-24 09:26:51 -0500

    update comment for MMSFormer terrain colors, and fix small bug causing sel_map to crash when segmentation bypass is enabled

--- sel_map_segmentation/sel_map_segmentation/src/sel_map_segmentation/BypassWrapper.py
+++ sel_map_segmentation/sel_map_segmentation/src/sel_map_segmentation/BypassWrapper.py
@@ -10,6 +10,6 @@ class BypassWrapper():
     def runSegmentation(self, pil_image, return_numpy=True, one_hot=False):
         width, height = pil_image.size
         if return_numpy:
-            return np.zeros((0, height, width))
+            return np.zeros((0, height, width)), 1 # confidence is 1 when there's no semantics
         else:
-            return torch.zeros((0, height, width), device=self.device)
+            return torch.zeros((0, height, width), device=self.device), 1 # confidence is 1 when there's no semantics
